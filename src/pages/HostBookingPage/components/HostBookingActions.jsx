bf514cdee6306a09ea48b4754ab6406f6ac98dad
karunya10 <karunya.prabha@gmail.com>
2025-09-16 20:03:53 +0200

    feat: update HostBookingActions to include view details button and change message host to listing details

--- src/pages/HostBookingPage/components/HostBookingActions.jsx
+++ src/pages/HostBookingPage/components/HostBookingActions.jsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { UserStar, Binoculars, MessageCircle, CircleX } from "lucide-react";
 
-export default function HostBookingActions({ onCancelBooking }) {
+export default function HostBookingActions({ onCancelBooking, onViewDetails }) {
   return (
     <>
       <div className="flex gap-2">
         <Button
-          title="Message Host"
+          title="Listing Details"
+          onClick={onViewDetails}
           className="rounded-xl px-6 py-2 text-blue-700 border-blue-200 bg-blue-50 hover:bg-blue-100 hover:scale-110  text-base font-medium"
         >
-          <MessageCircle />
+          <Binoculars />
         </Button>
         <Button
           title="Cancel Booking"
